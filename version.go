d714f4984f8c58b9ca2123f2fd50a1262e69be80
pjscruggs <PatrickJScruggs@gmail.com>
2025-04-25 20:13:00 -0500

    fix: update version to v0.1.0-alpha.1

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package slogcp
 
 // Version is the current version of the slogcp library.
 // Follows semantic versioning (https://semver.org/).
-const Version = "0.1.0"
+const Version = "v0.1.0-alpha.1"
 
 // UserAgent is the string sent with Cloud Logging API requests, identifying this library.
 const UserAgent = "slogcp/" + Version
